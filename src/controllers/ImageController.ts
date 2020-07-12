95ac2c59b7620e25e5f259eb35634bd2d6ec9450
Barreiradlc <augustodasilva53@gmail.com>
2020-07-12 01:20:36 -0300

    first commit

---
+++ src/controllers/ImageController.ts
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const { promisify } = require('util')
+const unlinkAsync = promisify(fs.unlink)
+import path from 'path';
+// 
+import { Request, Response } from 'express'
+
+class ImageController  {
+    upload( request: Request, response: Response ){        
+        const deleteImg = request.query.d
+        
+        if(deleteImg){
+            removeImg(String(deleteImg))
+        }
+        
+        return response.json({
+            img: request.file.filename
+        })
+
+        async function removeImg(deleteImg: string){                        
+            return await unlinkAsync(path.resolve(__dirname, '..', '..', 'uploads', deleteImg))
+        }
+    }
+
+
+    test( request: Request, response: Response ){
+        console.log('olar')
+
+        response.json([
+            'Alou'
+        ]);
+    }
+}
+
+export default ImageController
---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
+import ImageController from './controllers/ImageController'
+
+const imageController = new ImageController()
+
+const routes = express.Router()
+const upload = multer(multerConfig)
+
+
+routes.post('/img', upload.single('image'), imageController.upload)
+
+routes.get('/img', imageController.test)
+
+
+
+export default routes
