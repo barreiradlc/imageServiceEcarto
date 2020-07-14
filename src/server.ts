006d47b64e7c0152d2c212f6aa2df24959d88e8a
Barreiradlc <augustodasilva53@gmail.com>
2020-07-13 21:02:15 -0300

    Initializing project

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 import express, { response } from 'express'
 import routes from './routes'
 import path from 'path'
-
+console.log('a')
 const app = express()   
+console.log('b')
 
 app.use(express.json())
+console.log('c')
 app.use(routes)
+console.log('d')
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'))); 
+console.log('e')
 
 app.listen(3000)
+console.log('f')
