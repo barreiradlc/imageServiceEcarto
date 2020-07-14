570279da481687d3a9fa5df3f8bd0f0b9516eb7b
Barreiradlc <augustodasilva53@gmail.com>
2020-07-13 21:05:34 -0300

    Initializing project

--- src/controllers/ImageController.ts
+++ src/controllers/ImageController.ts
@@ -6,17 +6,22 @@ import path from 'path';
 import { Request, Response } from 'express'
 
 class ImageController  {
+
     upload( request: Request, response: Response ){        
+        console.log('p')
         const deleteImg = request.query.d
+        console.log('q')
         
         if(deleteImg){
             removeImg(String(deleteImg))
+            console.log('r')
         }
+        console.log('s')
         
         return response.json({
             img: request.file.filename
         })
-
+        
         async function removeImg(deleteImg: string){                        
             return await unlinkAsync(path.resolve(__dirname, '..', '..', 'uploads', deleteImg))
         }
--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,25 @@
 import express from 'express'
+console.log('g')
 import multer from 'multer'
+console.log('h')
 import multerConfig from './config/multer'
+console.log('i')
 import ImageController from './controllers/ImageController'
+console.log('j')
 
 const imageController = new ImageController()
+console.log('k')
 
 const routes = express.Router()
+console.log('l')
 const upload = multer(multerConfig)
+console.log('m')
 
 
 routes.post('/img', upload.single('image'), imageController.upload)
+console.log('n')
 routes.get('/img', imageController.test)
+console.log('o')
 
 
 
